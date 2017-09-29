1e705b464747d492c257874e8f8d4ba617e5d071
KeithCCC <keithc2che@gmail.com>
2017-09-30 08:09:15 +0900

    flask framework

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from wtforms import StringField, SubmitField, RadioField, SelectField
 from wtforms.validators import Required
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
+from wtforms import validators
 
 app = Flask(__name__)
 
@@ -11,12 +12,20 @@ app.secret_key = 'development key'
 Bootstrap(app)
 
 class mainform(Form):
+    Entry  = StringField('Enter data here', [validators.required()])
     submit = SubmitField('Submit')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     form = mainform()
-    return render_template('index.html', form=form)
+    result_text='result here'
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('index.html', form=form, result=result_text)
+
+@app.route("/success")
+def Success():
+    return 'Success'
 
 if __name__ == '__main__':
     app.run(debug=True)
